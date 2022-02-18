6317dda5beef5d1ceaee32f5159d9f1fac77426b
BrettLennox <brettheaton@hotmail.com>
2022-02-18 17:48:48 +1100

    AI Class 2
    created a simple player movement script and player gameobject.
    added waypoints for square to chase and a chase target when in range for the player

---
+++ Assets/Scripts/AIMovement.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIMovement : MonoBehaviour
+{
+    [Header("Goal")]
+    [Tooltip("The minimum distance to reach before stopping moving to the goal")]
+    public float minGoalDistance = 0.05f;
+    [Tooltip("The goal for the square to move towards")]
+    public GameObject goalPos0;
+    public Transform player;
+    [Tooltip("")]
+    public float chaseDistance = 3f;
+    public GameObject[] position;
+    public int positionIndex;
+
+    [Header("Speed")]
+    [Tooltip("The speed at which the AI moves")]
+    public float speed = 1.5f;
+
+    private void Update()
+    {
+        if(Vector2.Distance(transform.position, player.position) < chaseDistance)
+        {
+            AIMoveTowards(player);
+        }
+        else
+        {
+            AIMoveTowards(position[positionIndex].transform);
+        }
+    }
+
+    private void WaypointUpdate()
+    {
+        if (positionIndex < position.Length - 1)
+        {
+            positionIndex++;
+        }
+        else
+        {
+            positionIndex = 0;
+        }
+    }
+
+    private void AIMoveTowards(Transform goal)
+    {
+        #region Commented Out
+        //Vector2 AIPosition = transform.position;
+        //if (transform.position.x < position0.transform.position.x)
+        //{
+        //    AIPosition.x += (1 * Time.deltaTime);
+        //}
+        //else
+        //{
+        //    AIPosition.x -= (1 * Time.deltaTime);
+        //}
+        //if (transform.position.y < position0.transform.position.y)
+        //{
+        //    AIPosition.y += (1 * Time.deltaTime);
+        //}
+        //else
+        //{
+        //    AIPosition.y -= (1 * Time.deltaTime);
+        //}
+        //transform.position = AIPosition;
+
+
+        //transform.position = 
+        //    Vector2.MoveTowards(transform.position, position0.transform.position, 1 * Time.deltaTime);
+        #endregion
+        Vector2 AIPosition = transform.position;
+
+        //if we are not near the goal move towards
+        if (Vector2.Distance(AIPosition, goal.position) > minGoalDistance)
+        {
+            Vector2 directionToGoal = (goal.position - transform.position);
+            directionToGoal.Normalize();
+            transform.position += (Vector3)directionToGoal * speed * Time.deltaTime;
+        }
+        else
+        {
+            WaypointUpdate();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 3f;
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        Vector2 moveDir = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveDir.y++;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveDir.y--;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveDir.x++;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveDir.x--;
+        }
+        //moveDir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        //moveDir = transform.TransformDirection(moveDir);
+        moveDir *= (speed * Time.deltaTime);
+        transform.position += (Vector3)moveDir;
+    }
+}
